29b28421e6c0ac1e63332cb44d8d410a45df67da
jerry914 <joyce91400@hotmail.com>
2022-02-16 01:02:18 +0800

    vr mode suss

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
+  <div class="frame-container">
     <button @click="test">Change sphere color</button>
     <a-scene>
       <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
       <a-sphere position="0 1.25 -5" radius="1.25" color="blue"></a-sphere>
       <a-sky color="#ECECEC"></a-sky>
+      <a-entity id="rig" position="0 0 0">
+            <a-entity id="camera" camera look-controls></a-entity>
+        </a-entity>
     </a-scene>
   </div>
 </template>
@@ -29,5 +32,10 @@ export default {
   button {
     position: fixed;
     z-index: 9999;
+  }
+  .frame-container {
+    position: absolute; 
+    height: 100%; 
+    width: 100%;
   }
 </style>
